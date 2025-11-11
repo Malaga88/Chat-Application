03c93236deb883e63c1e5699cf35e3985e11ad70
darkshade <darkshade@kali.DarkShade>
2025-11-11 11:17:41 +0100

    Created Models

---
+++ model/message.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { create } = require('./user');
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', messageSchema);

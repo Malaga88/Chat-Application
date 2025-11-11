21fc0de355964366110a210c946223b8be83dcf8
darkshade <darkshade@kali.DarkShade>
2025-11-11 11:20:11 +0100

    added server details

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Define routes (example route)
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
